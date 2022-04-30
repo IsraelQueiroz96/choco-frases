f1229e4620d4be0af21f6c69411360f545708b94
Israel de Queiroz <103455169+IsraelQueiroz96@users.noreply.github.com>
2022-04-30 00:47:46 -0300

    Add files via upload

---
+++ mockup.js
@@ -0,0 +1,29 @@
+sortes = [
+    'Sem agonia és',
+    'Gabigol o príncipe do ninho do urubu',
+    'Dá pra mim não',
+    'Tive que catar as verduras todinha',
+    'Rafinha deve tá soando virado no caraí agora',
+    'Cala boca',
+    'Alô bolo de rolo',
+    'Vish maria',
+    'Tu é doido',
+    'Marcuuuuuuuus',
+    'Dale bambu de Junior Matuto',
+    'Oxi nada ver do caraí',
+    'Tem um cigarro aí?',
+    'Calor do caraí',
+    'Bora garimpar no rio',
+    'Eu vou ficar milionário',
+    'Resenha do caraí',
+    'O pirraia tá assistindo',
+    'Oxi cheio de problema Rafinha',
+    'Respeita o homem',
+    'Muito malvado esse Flamengo',
+    'Oxi esse chupa rola',
+    'Agora fudeo',
+    'Bora mengão porra',
+    
+];
+
+module.exports = sortes;
